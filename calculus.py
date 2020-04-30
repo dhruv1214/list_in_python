8af245d681a6ee9405f57ee29f5fcd835b079c55
Dhruv Desai <46900142+dhruv1214@users.noreply.github.com>
2020-04-30 17:37:35 +0530

    Delete calculus.py

--- calculus.py
+++
@@ -1,9 +0,0 @@
-import math
-def Calculus(x):
-    sine = math.sin(x)
-    cos = math.cos(x)
-    tan = math.tan(x)
-    return [sine,cos,tan]
-print("sine", Calculus(1))
-print("cos", Calculus(0))
-print("tan", Calculus(-1))
